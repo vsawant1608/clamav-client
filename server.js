0e4b30670d52afa89c18c0b7e47687ca6fb39068
Vishakha Sawant <visawant@in.ibm.com>
2023-08-22 19:34:48 +0530

    fix: push the app

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const app = express();
+const server = http.createServer(app)
+
+// See: http://expressjs.com/en/4x/api.html#app.settings.table
+const PRODUCTION = app.get('env') === 'production';
+
+// Administrative routes are not timed or logged, but for non-admin routes, pino
+// overhead is included in timing.
+app.get('/ready', (req, res) => res.status(200).json({ status: "ok" }));
+app.get('/live', (req, res) => res.status(200).json({ status: "ok" }));
+
+app.get('/', (req, res) => {
+    // Use req.log (a `pino` instance) to log JSON:
+    res.send('Hello from Node.js Starter Application!');
+});
+
+app.get('*', (req, res) => {
+    res.status(404).send("Not Found");
+});
+
+// Listen and serve.
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`App started on PORT ${PORT}`);
+});
